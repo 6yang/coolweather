7a8a3a3a07e7378196a937c34119b7d95436149b
e <tangshuixiaoer@163.com>
2019-10-29 21:40:33 +0800

    完成天气对应实体类的创建

---
+++ app/src/main/java/com/example/coolweather/entity/AQI.java
@@ -0,0 +1,13 @@
+package com.example.coolweather.entity;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
